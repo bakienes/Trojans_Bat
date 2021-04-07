47a10a856dd80d913cc151e64f281f5bd5670cb9
Baki Enes Yalçın <82122237+bakienes@users.noreply.github.com>
2021-04-08 01:42:15 +0300

    hackviews.bat

---
+++ hackviews.bat
@@ -0,0 +1,55 @@
+@echo off
+color 0a
+title Bey Bilisim ve Tasarim 
+
+echo          								`-:/++osssssso++/:-`                                        
+echo                                 `-/oydNNMMMMMMMMMMMMMMMMMMNNdyo/-`                                 
+echo                             ./sdNNMMMMmdhyo+//:::::://+oyhdmMMMMNNds/.                             
+echo                          -odNMMMNds+-.``                  ``.-+sdNMMMNdo-                          
+echo                       -smNMMNho-``                              ``-ohNMMNms-                       
+echo                    `+dNMMmy/.                 .://:.                 ./ymMMNd+`                    
+echo                  .omMMNh/`               `-:.:hdhhdd/:+-                `/hNMMmo.                  
+echo                .oNMMNs-                .+dN+ `.:ys:-` oNd/`                -sNMMNo.                
+echo              `+mMMNo.          `..-:/sdmMMN.   +NN/   `NMMmy+:.``            .oNMMm+`              
+echo             -hMMNy.          /dmmNNMMMMMMMy   --sd--   mMMMMMMNmddhyo.         .yNMMh-             
+echo            +NMMd:            mMMMMMMMMMMMMo     mM-`   mMMMMMMMMMMMMMd           :dMMN+            
+echo          `sMMMy`            /MMMMMMMMMMMMMh    :MMy   `NMMMMMMMMMMMMMM.           `yMMMs`          
+echo         `yMMN+              dMMMMMMMMMMMMMN`   oMMm   /MMMMMMMMMMMMMMM+             +NMMy`         
+echo        `hMMN/              -MMMMMMMMMMMMMMMo   hMMM` `mMMMMMMMMMMMMMMMs              /NMMh`        
+echo        yMMN/               oMMMMMMMMMMMMMMMN/  dMMM. oMMMMMMMMMMMMMMMMm.              /NMMy        
+echo       +MMM+                mMMMMMMMMMMMMMMMMN/ mMMM:+MMMMMMMMMMMMMMMMMN.               +MMM+       
+echo      .NMMh                /MMMMMMMMMMMMMMMMMMNomMMMhMMMMMMMMMMMMMMMMMMM/                hMMN.      
+echo      yMMN.                hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy                .NMMy      
+echo     .MMMs                 NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm                 sMMM.     
+echo     sMMM.                .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM`                .MMMs     
+echo     mMMd                 /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.                 dMMm     
+echo    `MMMs                 oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM/                 sMMM`  
+echo    mMMdm			          Bey Bilisim ve Hack				  mMMd
+echo    -MMM+                 hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                 +MMM-    
+echo    -MMM/                 +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                 /MMM-    
+echo    -MMM+                  /NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:                 +MMM-    
+echo    `MMMs                   .yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNs-                  sMMM`    
+echo     mMMd                     .omMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNy/`                    dMMm     
+echo     sMMM.                      `oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs:`                      .MMMs     
+echo     .MMMs                       +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN                         sMMM.     
+echo      yMMN.                      hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM`                       .NMMy      
+echo      .NMMy                      NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:                       yMMN.      
+echo       +MMM+                    -MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+                      +MMM+       
+echo        yMMN/                   oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                     /NMMy        
+echo        `hMMN/                  hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh                    /NMMh`        
+echo         `yMMN+                 mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN                   +NMMy`         
+echo          `sMMMy`               -/mMMMMMMMMMMMMMMMMMMMMMMMMMMMMmys                 `yMMMs`          
+echo            +NMMd:                yMMMMMMMMMMMMMMMMMMMMMMMMMMMM+                  :dMMN+            
+echo             -hMMNy.              oMMMMMMMMMMMMMMMMMMMMMMMMMMMM-                .yNMMh-             
+echo              `+mMMNo.            /MMMMMMMMMMMMMMMMMMMMMMMMMMMM`              .oNMMm+`              
+echo                .oNMMNs-          .MMMMMMMMMMMMMMMMMMMMMMMMMMMm             -sNMMNo.                
+echo                 .omMMNh/`        NMMMMMMMMMMMMMMMMMMMMMMMMMMy          `/hNMMmo.                  
+echo                    `+dNMMmy/`     hMMMMMMMMMMMMMMMMMMMMMMMMMM+       `/ymMMNd+`                    
+echo                       -smNMMNho-``-oydmNNMMMMMMMMMMMMMMMMNNmd.  ``-ohNMMNms-                       
+echo                          :odNMMMNhs+-..-::/+osssyyssoo+/:-.`.-+shNMMMNdo:                          
+echo                             ./sdNNMMMMmdhyo+//:::::://+oyhdmMMMMNNds/.                             
+echo                                 `-/oydNNMMMMMMMMMMMMMMMMMMNNdyo/-`                                 
+echo                                        `-:/++osssssso++/:-`                       
+
+pause >nul
+start cmd.exe
